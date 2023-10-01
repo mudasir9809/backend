5ee59e561a467ba4ea9a66347c8d2cff740430bc
mudasir9809 <99568056+mudasir9809@users.noreply.github.com>
2023-10-01 00:56:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,32 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const Facebook = require('./models/Facebook')
-const Twitter = require('./models/Twitter')
-const Phone = require('./models/Phone')
-const Gmail = require('./models/Gmail')
+const User = require('./models/User')
+
 const app = express()
 const port = 3000
 let uri = 'mongodb+srv://rogue2:rogue4554@cluster0.e24lw6l.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(uri)
 var corsOptions = {
-    origin: 'http://127.0.0.1:5500',
+    origin: '*',
     optionsSuccessStatus: 200
 }
 
 app.use(cors())
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('success')
-})
-app.post('/login', (req, res) => {
-    console.log(req.body)
-    const { type, email, phone, password } = req.body;
-    try {
-        switch (type) {
-            case 'facebook':
-                Facebook.create({
-                    email: email,
-                    password: password
-                })
-                break;
-            case 'twitter':
-                Twitter.create({
-                    email: email,
-                    password: password
-                })
-                break;
-            case 'gmail':
-                Gmail.create({
-                    email: email,
-                    password: password
-                })
-                break;
-            case 'phone':
-                Phone.create({
-                    phone: phone,
-                    password: password
-                })
-                break;
-        }
 
+
+app.get('/', async (req, res) => {
+    let all = await User.find({})
+
+    res.status(200).json({all:all})
+})
+
+app.post('/data', (req, res) => {
+    const { name, crush } = req.body;
+    User.create({name:name, crush:crush})
+    try {
+        
         res.status(201).json({ success: true, message: "Invalid phone/email or password" })
     } catch (error) {
         console.log(error)
