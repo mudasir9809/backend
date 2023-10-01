cc9e8d51d647885ce586874adacc4fde73bb79e3
mudasir9809 <99568056+mudasir9809@users.noreply.github.com>
2023-10-01 01:03:27 -0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    name:{type:String},
+    crush: {type: String}
+})
+
+module.exports = mongoose.model('User', userSchema)
