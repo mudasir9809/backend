d8a902ed39df6756793e7c1790172e546bdeed9b
mudasir9809 <99568056+mudasir9809@users.noreply.github.com>
2023-10-02 03:05:36 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 const userSchema = new Schema({
     name:{type:String},
-    crush: {type: String}
+    crush: {type: String},
+    value: {type:String}
 })
 
 module.exports = mongoose.model('User', userSchema)
